ff2ffd23336fb9f8d5ccd5b058b06d2a207f7628
Sergei Popov <sergei.v.popov@icloud.com>
2023-12-12 02:04:23 +0300

    add homework 6

--- src/video.py
+++ src/video.py
@@ -1,23 +1,37 @@
 from datetime import timedelta
-from src.channel import YouTubeObject
+
 from isodate import parse_duration
+
+from src.channel import YouTubeObject
+from src.utils import printj
 
 
 class Video(YouTubeObject):
     def __init__(self, video_id: str) -> None:
         self.__video_id = video_id
-        self.__url = f'https://youtu.be/{video_id}'
 
         video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                  id=video_id
                                                  ).execute()
-        self.__title = video['items'][0]['snippet']['title']
-        self.__description = video['items'][0]['snippet']['description']
-        self.__video_title: str = video['items'][0]['snippet']['title']
-        self.__view_count: int = video['items'][0]['statistics']['viewCount']
-        self.__like_count: int = video['items'][0]['statistics']['likeCount']
-        self.__comment_count: int = video['items'][0]['statistics']['commentCount']
-        self.__duration_iso8601 = video['items'][0]['contentDetails']['duration']
+        try:
+            video_item = video['items'][0]
+            self.__url = f'https://youtu.be/{video_id}'
+            self.__title = video_item['snippet']['title']
+            self.__description = video_item['snippet']['description']
+            self.__video_title: str = video_item['snippet']['title']
+            self.__view_count: int = video_item['statistics']['viewCount']
+            self.__like_count: int = video_item['statistics']['likeCount']
+            self.__comment_count: int = video_item['statistics']['commentCount']
+            self.__duration_iso8601 = video_item['contentDetails']['duration']
+        except IndexError:
+            self.__url = None
+            self.__title = None
+            self.__description = None
+            self.__video_title = None
+            self.__view_count = None
+            self.__like_count = None
+            self.__comment_count = None
+            self.__duration_iso8601 = None
 
     @property
     def title(self):
